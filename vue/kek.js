a7304d05586543db2b848f3fd79a48b2851e95ed
Sergey <sp@outsi.de>
2022-01-07 17:37:20 +0300

    UI game

--- vue/kek.js
+++ vue/kek.js
@@ -101,16 +101,31 @@ const map = [[9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,
 
 
 const html_map = document.getElementById('map');
-
+const html_vector = document.getElementById('vector');
+const html_radius = document.getElementById('radius');
 const santa = document.createElement("div");
 santa.className = "santa";
-santa.innerHTML = '🎅';
+let clickableArea = [];
+
+const RADIUS = {"9": -1, "0": 0, "1": 1, "2": 2, "4": 2, "3": 2}
+
+
 
 santaX = 57;
-santaY = 50;
+santaY = 49;
 
-function createBlock(item, i, j) {
-    let div = document.createElement("div")
+let vector = [0, 0];
+let radius;
+
+let gifts = 0;
+let points = 0;
+let steps = 0;
+
+
+function createBlock(item, y, x) {
+    let div = document.createElement("button");
+    div.setAttribute('data-x', x);
+    div.setAttribute('data-y', y);
     // div.innerHTML = item;
     switch (item) {
         case 0:
@@ -123,9 +138,8 @@ function createBlock(item, i, j) {
             div.className = "cell road";
             break;
         case 3:
-            div.className = "cell santa-cell";
+            div.className = "cell santa";
             div.appendChild(santa);
-            console.log(i, j);
             break;
         case 4:
             div.className = "cell gift";
@@ -146,21 +160,83 @@ for (let i in map) {
     html_map.appendChild(row);
 }
 
-const buttonN = document.getElementById("N");
 
 
 
-buttonN.addEventListener('click', function() {
-    let x = santaX;
-    let y = santaY;
-    santaY -= 1;
-    moveSanta(x, y);
-})
+radius = RADIUS[map[santaY][santaX]];
+$('#santa')[0].innerHTML = `${santaX}, ${santaY}`;
+
+
+html_radius.innerHTML = radius;
+html_vector.innerHTML = vector;
+$('#gifts')[0].innerHTML = gifts;
+$('#points')[0].innerHTML = points;
+$('#steps')[0].innerHTML = steps;
+
+
+makeClickable();
+gameHasStarted = true;
 
 function moveSanta(x, y) {
-    console.log(x, y, santaX, santaY);
-    console.log(html_map, html_map.childNodes[x].childNodes[y])
-    html_map.childNodes[y].childNodes[x].classList.remove('santa');
-    html_map.childNodes[santaY].childNodes[santaX].classList.add('santa');
+    html_map.childNodes[y].childNodes[x].classList.add('santa');
+    html_map.childNodes[santaY].childNodes[santaX].classList.remove('santa');
+    santaX = x;
+    santaY = y;
+    steps++;
+    $('#steps')[0].innerHTML = steps;
+    $('#santa')[0].innerHTML = `${santaX}, ${santaY}`;
+    if (map[y][x] === 4) {
+        gifts++;
+        $('#gifts')[0].innerHTML = gifts;
+        $(html_map.childNodes[y].childNodes[x]).removeClass('gift');
+        map[y][x] = 2;
+        $(html_map.childNodes[y].childNodes[x]).addClass('road');
+    }
+    $('#points')[0].innerHTML = 3600 * (Math.pow(1.1, gifts)) / steps;
 }
+
+function makeClickable() {
+    $(':button').prop('disabled', true); // Disable all the buttons
+    const size = radius * 2 + 1;
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let cell = html_map.childNodes[santaY - radius + vector[1] + i].childNodes[santaX - radius + vector[0] + j];
+            if (cell && map[cell.getAttribute('data-y')][cell.getAttribute('data-x')] !== 9) {
+                $(cell).addClass('clickable');
+                $(cell)[0].removeAttribute('disabled');
+            }
+        }
+    }
+    if (!$('.clickable').length) {
+        $('#gameover').modal();
+    }
+}
+
+function clearClickable() {
+    $('.clickable').removeClass('clickable');
+}
+
+$('#gameover').on('hidden.bs.modal', function() {
+    console.log('close');
+});
+
+
+
+function getMovements() {
+    
+}
+
+
+$('.cell').on('click', function() {
+    let x = $(this).attr('data-x');
+    let y = $(this).attr('data-y');
+    vector[0] = x - santaX;
+    vector[1] = y - santaY;
+    html_vector.innerHTML = vector;
+    clearClickable();
+    moveSanta(x, y);
+    radius = RADIUS[map[santaY][santaX]];
+    html_radius.innerHTML = radius;
+    makeClickable();
+})
 
