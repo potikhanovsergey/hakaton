d845b79722b174118bbdb51cb35fbbee82702594
Sergey <sp@outsi.de>
2022-01-08 00:32:16 +0300

    undo logic

--- vue/kek.js
+++ vue/kek.js
@@ -621,11 +621,30 @@ let gifts = 0;
 let points = 0;
 let steps = 0;
 
+
+let history = [];
+let stepInHistory = 0;
+
+let INFORMATION = {
+    santaX: 57,
+    santaY: 49,
+    vector: [0, 0],
+    gifts: 0,
+    points: 0,
+    steps: 0,
+    radius: null
+}
+
+INFORMATION.radius = RADIUS[map[INFORMATION.santaY][INFORMATION.santaX]];
+
+history.push(JSON.parse(JSON.stringify(INFORMATION)));
+
+console.log(history);
+
 function createBlock(item, y, x) {
   let div = document.createElement("button");
   div.setAttribute("data-x", x);
   div.setAttribute("data-y", y);
-  // div.innerHTML = item;
   switch (item) {
     case 0:
       div.className = "cell ice";
@@ -659,29 +678,31 @@ for (let i in map) {
   html_map.appendChild(row);
 }
 
-radius = RADIUS[map[santaY][santaX]];
-$("#santa")[0].innerHTML = `${santaX}, ${santaY}`;
+radius = RADIUS[map[santaY][santaX]]; // undo
 
-html_radius.innerHTML = radius;
-html_vector.innerHTML = vector;
-$("#gifts")[0].innerHTML = gifts;
-$("#points")[0].innerHTML = points;
-$("#steps")[0].innerHTML = steps;
+$("#santa")[0].innerHTML = `${INFORMATION.santaX}, ${INFORMATION.santaY}`;
+
+html_radius.innerHTML = INFORMATION.radius;
+html_vector.innerHTML = INFORMATION.vector;
+$("#gifts")[0].innerHTML = INFORMATION.gifts;
+$("#points")[0].innerHTML = INFORMATION.points;
+$("#steps")[0].innerHTML = INFORMATION.steps;
 
 makeClickable();
 gameHasStarted = true;
 
+
 function moveSanta(x, y) {
   html_map.childNodes[y].childNodes[x].classList.add("santa");
-  html_map.childNodes[santaY].childNodes[santaX].classList.remove("santa");
-  santaX = x;
-  santaY = y;
-  steps++;
-  $("#steps")[0].innerHTML = steps;
-  $("#santa")[0].innerHTML = `${santaX}, ${santaY}`;
+  html_map.childNodes[INFORMATION.santaY].childNodes[INFORMATION.santaX].classList.remove("santa");
+
+  INFORMATION.santaX = +x;
+  INFORMATION.santaY = +y;
+  $("#steps")[0].innerHTML = INFORMATION.steps;
+  $("#santa")[0].innerHTML = `${INFORMATION.santaX}, ${INFORMATION.santaY}`;
   if (map[y][x] === 4) {
-    gifts++;
-    $("#gifts")[0].innerHTML = gifts;
+    INFORMATION.gifts += 1;
+    $("#gifts")[0].innerHTML = INFORMATION.gifts;
     $(html_map.childNodes[y].childNodes[x]).removeClass("gift");
     map[y][x] = 2;
     $(html_map.childNodes[y].childNodes[x]).addClass("road");
@@ -689,17 +710,29 @@ function moveSanta(x, y) {
         $("#gameover").modal();
     }
   }
-  $("#points")[0].innerHTML = (3600 * Math.pow(1.1, gifts)) / steps;
+  $("#points")[0].innerHTML = (3600 * Math.pow(1.1, INFORMATION.gifts)) / INFORMATION.steps; // undo
+  INFORMATION.points = (3600 * Math.pow(1.1, INFORMATION.gifts)) / INFORMATION.steps;
+  history = history.slice(0, stepInHistory + 1);
+  history.push({
+      santaX: INFORMATION.santaX,
+      santaY: INFORMATION.santaY,
+      vector: [...INFORMATION.vector],
+      gifts: INFORMATION.gifts,
+      points: INFORMATION.points,
+      steps: INFORMATION.steps,
+      radius: INFORMATION.radius
+  });
+  stepInHistory = history.length - 1;
 }
 
 function makeClickable() {
   $(".cell").prop("disabled", true); // Disable all the buttons
-  const size = radius * 2 + 1;
+  const size = INFORMATION.radius * 2 + 1; // undo
   for (let i = 0; i < size; i++) {
     for (let j = 0; j < size; j++) {
       let cell =
-        html_map?.childNodes[santaY - radius + vector[1] + i]
-        ?.childNodes[santaX - radius + vector[0] + j];
+        html_map?.childNodes[INFORMATION.santaY - INFORMATION.radius + INFORMATION.vector[1] + i]
+        ?.childNodes[INFORMATION.santaX - INFORMATION.radius + INFORMATION.vector[0] + j];
       if (
         cell &&
         map[cell.getAttribute("data-y")][cell.getAttribute("data-x")] !== 9
@@ -728,19 +761,29 @@ $(document).on($.modal.OPEN, () => {
   //location.reload();
 });
 
-function getMovements() {}
+
+function updateStats() {
+    $("#steps")[0].innerHTML = INFORMATION.steps;
+    $("#santa")[0].innerHTML = `${INFORMATION.santaX}, ${INFORMATION.santaY}`;
+    $("#points")[0].innerHTML = INFORMATION.steps ? (3600 * Math.pow(1.1, INFORMATION.gifts)) / INFORMATION.steps : 0; // undo
+    $("#gifts")[0].innerHTML = INFORMATION.gifts;
+    html_radius.innerHTML = INFORMATION.radius;
+    html_vector.innerHTML = INFORMATION.vector;
+    console.log('updated');
+}
 
 $(".cell").on("click", function () {
   let x = $(this).attr("data-x");
   let y = $(this).attr("data-y");
-  vector[0] = x - santaX;
-  vector[1] = y - santaY;
-  html_vector.innerHTML = vector;
-  result.path.push([...vector]);
+  INFORMATION.vector[0] = x - INFORMATION.santaX;
+  INFORMATION.vector[1] = y - INFORMATION.santaY;
+  html_vector.innerHTML = INFORMATION.vector;
+  result.path.push([...INFORMATION.vector]);
   clearClickable();
+  INFORMATION.steps += 1;
   moveSanta(x, y);
-  radius = RADIUS[map[santaY][santaX]];
-  html_radius.innerHTML = radius;
+  INFORMATION.radius = RADIUS[map[INFORMATION.santaY][INFORMATION.santaX]];
+  html_radius.innerHTML = INFORMATION.radius;
   makeClickable();
 });
 
@@ -751,4 +794,19 @@ $('#copy-json').on('click', function() {
     $temp.val(data).select();
     document.execCommand("copy");
     $temp.remove();
+});
+
+
+$('#undo').on('click', function() {
+    if (stepInHistory > 0) {
+        let y = INFORMATION.santaY;
+        let x = INFORMATION.santaX;
+        stepInHistory -= 1;
+        INFORMATION = history[stepInHistory];
+        updateStats();
+        clearClickable();
+        html_map.childNodes[y].childNodes[x].classList.remove("santa");
+        html_map.childNodes[INFORMATION.santaY].childNodes[INFORMATION.santaX].classList.add("santa");
+        makeClickable();
+    }
 });
